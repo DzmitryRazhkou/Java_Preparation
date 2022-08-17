2cbecf6b1718d7e146b337b640a5119c63db09d1
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-08-17 14:22:06 -0700

    ReverseArray

--- src/main/java/preparation/integer/ReverseArray.java
+++ src/main/java/preparation/integer/ReverseArray.java
@@ -4,14 +4,22 @@ import java.util.Arrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int[]as = {1, 9, 7, 12, 90, 125};
-        for (int i = as.length-1; i >= 0; i--) {
-            System.out.println(as[i]);
-        }
-
-        int index = 3;
-        int value = 199;
-        as[index] = value;
-        System.out.println(Arrays.toString(as));
+        int[] as = {1, 9, 7, 12, 90, 125};
+        reverse(as, as.length);
     }
+
+    public static void reverse(int[] arr, int n) {
+        int[] rev_arr = new int[n];
+        int j = n;
+
+        for (int i = 0; i < n; i++) {
+            rev_arr[j - 1] = arr[i];
+            j = j - 1;
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.println(rev_arr[k]);
+        }
+    }
+
 }
+
